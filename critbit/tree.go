57b6bec35785eb7e703a82397667cfc2098712a6
Burgett, Gordon <Gordon.Burgett@concur.com>
2015-04-26 17:56:45 -0400

    Initial critbit tree implementation with Get and Set

---
+++ critbit/tree.go
@@ -0,0 +1,215 @@
+package critbit
+
+import "bytes"
+
+// A copy-on-write critbit Trie.  It stores key-value pairs where the key is a byte slice.
+// The internal implementation is based on https://github.com/agl/critbit/blob/master/critbit.pdf
+type Trie struct {
+	root  *node
+	count uint32
+}
+
+type node struct {
+	// the index into the byte array of the byte containing the critical bit.
+	critbyte int
+	// a bitmask where all bits except the critical bit are true.  This allows more efficient
+	// identification of the critical bit.
+	critbit uint8
+
+	children [2]*node
+
+	//the key at this leaf.  All external leafs have a non-nil key.
+	key []byte
+	//The value at this leaf.  All external leafs have a non-nil value, and do not point to other nodes.
+	value interface{}
+}
+
+var nilTrie *Trie = &Trie{
+	root:  nil,
+	count: 0,
+}
+
+// Gets the singleton nil trie.  It is a singleton because it is immutable.
+func NilTrie() *Trie {
+	return nilTrie
+}
+
+//-- read operations --//
+
+// Tests whether the trie contains the given key.
+func (t *Trie) ContainsKey(key []byte) bool {
+	if t.root == nil {
+		return false
+	}
+
+	n := t.root.findBestLeaf(key)
+	if bytes.Equal(key, n.key) {
+		return true
+	}
+	return false
+}
+
+// Gets the number of items in the tree.
+func (t *Trie) Len() uint32 {
+	return t.count
+}
+
+//-- write operations --//
+
+// Returns a new Trie with the given key set to the given value.
+func (t *Trie) Set(key []byte, value interface{}) (*Trie, interface{}) {
+	if value == nil {
+		panic("value cannot be nil")
+	}
+
+	if t.root == nil {
+		return &Trie{
+			root: &node{
+				key:   key,
+				value: value,
+			},
+			count: 1,
+		}, nil
+	}
+
+	n := t.root.findBestLeaf(key)
+	if bytes.Equal(key, n.key) {
+		return &Trie{
+			root:  t.root.setLeaf(key, value),
+			count: t.count,
+		}, n.value
+	}
+
+	//insert node
+	critbyte, critbit := findCritbit(key, n.key)
+	return &Trie{
+		root:  t.root.insertLeaf(key, value, critbyte, critbit),
+		count: t.count + 1,
+	}, nil
+}
+
+//-- internal functions --//
+
+func (n *node) findBestLeaf(key []byte) *node {
+	if n.key != nil {
+		//it's a leaf - return it
+		return n
+	}
+
+	direction := findDirection(key, n.critbyte, n.critbit)
+	return n.children[direction].findBestLeaf(key)
+}
+
+func (n *node) setLeaf(key []byte, value interface{}) *node {
+	if n.key != nil {
+		//it's the leaf - set it
+		return &node{
+			key:   key,
+			value: value,
+		}
+	}
+
+	//walk the tree, and create a new node to return pointing to our new deep child.
+	direction := findDirection(key, n.critbyte, n.critbit)
+	ret := &node{
+		critbit:  n.critbit,
+		critbyte: n.critbyte,
+	}
+	ret.children[1-direction] = n.children[1-direction]
+	ret.children[direction] = n.children[direction].setLeaf(key, value)
+	return ret
+}
+
+func (n *node) insertLeaf(key []byte, value interface{}, critbyte int, critbit uint8) *node {
+	if n == nil {
+		panic("nil node")
+	}
+
+	if n.key != nil ||
+		n.critbyte > critbyte || (n.critbyte == critbyte && n.critbit < critbit) {
+		//this is the leaf we calculated the critbit from OR
+		//this node's critbit is bigger than the one we're trying to add, add a node before it
+		dir := findDirection(key, critbyte, critbit)
+		ret := &node{
+			critbyte: critbyte,
+			critbit:  critbit,
+		}
+		ret.children[dir] = &node{
+			key:   key,
+			value: value,
+		}
+		ret.children[1-dir] = n
+		return ret
+	}
+
+	//this node's critbit is smaller than the one we're trying to add, insert after it
+	dir := findDirection(key, n.critbyte, n.critbit)
+	ret := &node{
+		critbyte: n.critbyte,
+		critbit:  n.critbit,
+	}
+	ret.children[dir] = n.children[dir].insertLeaf(key, value, critbyte, critbit)
+	ret.children[1-dir] = n.children[1-dir]
+	return ret
+}
+
+func findDirection(key []byte, critbyte int, critbit uint8) int {
+	if critbit == 255 {
+		//special case - length comparison
+		if critbyte == len(key) {
+			return 0
+		}
+		return 1
+	}
+	//identify correct child
+	c := key[critbyte]
+	r := (1 + (critbit | c)) >> 7
+	return 1 - int(r)
+}
+
+func findCritbit(u []byte, p []byte) (int, uint8) {
+	//find the critical byte
+
+	found := false
+	var newbyte int
+	var newcritbit uint8
+
+	//search through our new key u to find the first differing byte
+	for newbyte = 0; newbyte < len(u); newbyte++ {
+
+		if newbyte >= len(p) {
+			return len(p), 255 //special case - u is longer
+		}
+		if p[newbyte] != u[newbyte] {
+			newcritbit = p[newbyte] ^ u[newbyte]
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return len(u), 255 //special case - p is longer
+	}
+
+	//find the critical bit
+	newcritbit |= newcritbit >> 1
+	newcritbit |= newcritbit >> 2
+	newcritbit |= newcritbit >> 4
+
+	//at this point all bits including and below the highest bit are set
+	//xor to unset all but the highest bit
+	newcritbit = newcritbit ^ (newcritbit >> 1)
+	//invert to create our critbit mask
+	newcritbit = ^newcritbit
+
+	//critbits (lowest to highest): fe, fd, fb, f7, ef, df, bf, 7f
+
+	return newbyte, newcritbit
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ critbit/tree_test.go
@@ -0,0 +1,225 @@
+package critbit
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSet_Insert_Root(t *testing.T) {
+	instance := NilTrie()
+
+	//act
+	result, old := instance.Set([]byte{0x01, 0x02, 0x03}, 123)
+
+	//assert
+	assert.Nil(t, old, "nothing should exist in tree")
+	assert.NotEqual(t, result, instance, "result should be new list")
+	assert.Equal(t, 1, result.Len(), "result should have length 1")
+	assert.NotNil(t, result.root, "result should have a root node")
+	assert.Equal(t, []byte{0x01, 0x02, 0x03}, result.root.key, "root node should be the leaf")
+	assert.Equal(t, 123, result.root.value, "root node should be the leaf")
+
+	assert.Equal(t, 0, instance.Len(), "original tree should remain immutable")
+	assert.Nil(t, instance.root, "original tree should remain immutable")
+}
+
+func TestSet_Insert_SecondNodeAtZero(t *testing.T) {
+	instance, _ := NilTrie().Set([]byte{0x01, 0x02, 0x03}, 123)
+
+	//act
+	result, old := instance.Set([]byte{0x01, 0x02, 0x02}, 122)
+
+	//assert
+	assert.Nil(t, old, "nothing should be overwritten")
+	assert.NotEqual(t, result, instance, "result should be new list")
+	assert.Equal(t, 2, result.Len(), "result should have length 1")
+	assert.Nil(t, result.root.key, "root node should be a critbit node")
+	assert.Equal(t, 2, result.root.critbyte, "root critbyte")
+	assert.Equal(t, 0xFE, result.root.critbit, "root critbit")
+	assert.Equal(t, []byte{0x01, 0x02, 0x03}, result.root.children[1].key, "first child key")
+	assert.Equal(t, []byte{0x01, 0x02, 0x02}, result.root.children[0].key, "zero child key")
+
+	assert.Equal(t, 1, instance.Len(), "original tree should remain immutable")
+	assert.Equal(t, []byte{0x01, 0x02, 0x03}, instance.root.key, "original tree should remain immutable")
+}
+
+func TestSet_Insert_SecondNodeAtOne(t *testing.T) {
+	instance, _ := NilTrie().Set([]byte{0x01, 0x02, 0x03}, 123)
+
+	//act
+	result, old := instance.Set([]byte{0x01, 0x04, 0x02}, 142)
+
+	//assert
+	assert.Nil(t, old, "nothing should be overwritten")
+	assert.NotEqual(t, result, instance, "result should be new list")
+	assert.Equal(t, 2, result.Len(), "result should have length 1")
+	assert.Nil(t, result.root.key, "root node should be a critbit node")
+	assert.Equal(t, 1, result.root.critbyte, "root critbyte")
+	assert.Equal(t, 0xFB, result.root.critbit, "root critbit")
+	assert.Equal(t, []byte{0x01, 0x04, 0x02}, result.root.children[1].key, "first child key")
+	assert.Equal(t, []byte{0x01, 0x02, 0x03}, result.root.children[0].key, "zero child key")
+
+	assert.Equal(t, 1, instance.Len(), "original tree should remain immutable")
+	assert.Equal(t, []byte{0x01, 0x02, 0x03}, instance.root.key, "original tree should remain immutable")
+}
+
+func TestSet_Insert_ThirdNodeBeforeNode(t *testing.T) {
+	instance, _ := NilTrie().Set([]byte{0x01, 0x02, 0x03}, 123)
+	instance, _ = instance.Set([]byte{0x01, 0x02, 0x04}, 123)
+
+	//act
+	result, old := instance.Set([]byte{0x01, 0x80, 0x02}, 182)
+
+	//assert
+	assert.Nil(t, old, "nothing should be overwritten")
+	assert.NotEqual(t, result, instance, "result should be new list")
+	assert.Equal(t, 3, result.Len(), "result should have length 1")
+	assert.Nil(t, result.root.key, "root node should be a critbit node")
+	assert.Equal(t, 1, result.root.critbyte, "root critbyte")
+	assert.Equal(t, 0x7F, result.root.critbit, "root critbit")
+	assert.Equal(t, []byte{0x01, 0x80, 0x02}, result.root.children[1].key, "first child key")
+	assert.Nil(t, result.root.children[0].key, "zero child should be node")
+
+}
+
+func TestSet_Insert_ThirdNodeAfterNode(t *testing.T) {
+	instance, _ := NilTrie().Set([]byte{0x01, 0x02, 0x03}, 123)
+	instance, _ = instance.Set([]byte{0x01, 0x01, 0x04}, 123)
+
+	//act
+	result, old := instance.Set([]byte{0x01, 0x02, 0x02}, 182)
+
+	//assert
+	assert.Nil(t, old, "nothing should be overwritten")
+	assert.NotEqual(t, result, instance, "result should be new list")
+	assert.Equal(t, 3, result.Len(), "result should have length 1")
+	assert.Nil(t, result.root.key, "root node should be a critbit node")
+	assert.Equal(t, 1, result.root.critbyte, "root critbyte")
+	assert.Equal(t, 0xFD, result.root.critbit, "root critbit")
+	assert.Equal(t, []byte{0x01, 0x01, 0x04}, result.root.children[0].key, "0 child key")
+	assert.Nil(t, result.root.children[1].key, "1 child should be node")
+	assert.Equal(t, 2, result.root.children[1].critbyte, "child critbyte")
+	assert.Equal(t, 0xFE, result.root.children[1].critbit, "child critbit")
+
+}
+
+//-- Internal functions --//
+
+func TestFindCritbit_LowestBit(t *testing.T) {
+	u := []byte{0x00, 0x00, 0x00, 0x00}
+	p := []byte{0x01, 0x00, 0x00, 0x00}
+
+	//act
+	critbyte, critbit := findCritbit(u, p)
+
+	//assert
+	assert.Equal(t, 0, critbyte, "critbyte should be initial byte")
+	assert.Equal(t, 0xFE, critbit, "1111 1110")
+}
+
+func TestFindCritbit_HighestBit(t *testing.T) {
+	u := []byte{0x27, 0x00, 0x00, 0x00} // 0000 0011
+	p := []byte{0x27, 0x80, 0x00, 0x00} // 0000 0000
+
+	//act
+	critbyte, critbit := findCritbit(u, p)
+
+	//assert
+	assert.Equal(t, 1, critbyte, "critbyte should be second byte")
+	assert.Equal(t, 0x7F, critbit, "1101 1111")
+}
+
+func TestFindCritbit_AllBits(t *testing.T) {
+	for i := uint8(0); i < 8; i++ {
+		for j := 0x01 << i; j < 0x01<<(i+1); j++ {
+			//test every possible bitmask combination having critbit at position i
+			u := []byte{0x01 << i}
+			p := []byte{0x00}
+
+			//act
+			_, critbit := findCritbit(u, p)
+			_, critbit2 := findCritbit(p, u)
+
+			//assert
+			assert.Equal(t, ^(uint8(0x01) << i), critbit, fmt.Sprintf("u: %x p: %x index: %d", u[0], p[0], i))
+			assert.Equal(t, ^(uint8(0x01) << i), critbit2, fmt.Sprintf("u: %x p: %x index: %d", u[0], p[0], i))
+		}
+	}
+}
+
+func TestFindCritbit_PIsLonger_CritbitIs255(t *testing.T) {
+	u := []byte{0x27, 0x00, 0x00, 0x94}
+	p := []byte{0x27, 0x00, 0x00}
+
+	//act
+	critbyte, critbit := findCritbit(u, p)
+
+	//assert
+	assert.Equal(t, 3, critbyte, "critbyte should be length of p")
+	assert.Equal(t, 0xFF, critbit, "255")
+}
+
+func TestFindCritbit_UIsLonger_CritbitIs255(t *testing.T) {
+	u := []byte{0x27, 0x00, 0x00, 0x94}
+	p := []byte{0x27, 0x00, 0x00, 0x94, 0x00}
+
+	//act
+	critbyte, critbit := findCritbit(u, p)
+
+	//assert
+	assert.Equal(t, 4, critbyte, "critbyte should be length of u")
+	assert.Equal(t, 0xFF, critbit, "255")
+}
+
+func TestFindDirection_InitialByte_ComparesCorrectBit(t *testing.T) {
+	u := []byte{0x80, 0x00}
+	p := []byte{0x00, 0x00}
+
+	critbyte := 0
+	critbit := uint8(0x7F)
+
+	//act
+	directionU := findDirection(u, critbyte, critbit)
+	directionP := findDirection(p, critbyte, critbit)
+
+	//assert
+	assert.Equal(t, 1, directionU, "0x80")
+	assert.Equal(t, 0, directionP, "0x00")
+}
+
+func TestFindDirection_LastByte_ComparesCorrectBit(t *testing.T) {
+	u := []byte{0x00, 0x01}
+	p := []byte{0x00, 0x80}
+
+	critbyte := 1
+	critbit := uint8(0x01)
+
+	//act
+	directionU := findDirection(u, critbyte, critbit)
+	directionP := findDirection(p, critbyte, critbit)
+
+	//assert
+	assert.Equal(t, 1, directionU, "0x01")
+	assert.Equal(t, 0, directionP, "0x80")
+}
+
+func TestFindDirection_AllBits_ComparesCorrectBit(t *testing.T) {
+	u := []byte{0xFF}
+	p := []byte{0x00}
+
+	for i := uint8(0); i < 8; i++ {
+		critbyte := 0
+		critbit := ^(uint8(0x01) << i)
+
+		//act
+		directionU := findDirection(u, critbyte, critbit)
+		directionP := findDirection(p, critbyte, critbit)
+
+		//assert
+		assert.Equal(t, 1, directionU, "0x01")
+		assert.Equal(t, 0, directionP, "0x80")
+
+	}
+}
